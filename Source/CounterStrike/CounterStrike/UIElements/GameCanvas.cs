8e3a275bd915e4a2902b75dde4dcab78cbd49288
Danylo <goddan333@gmail.com>
2016-01-18 18:20:27 +0200

    Revert "create Game Canvas, add dep prop"
    This reverts commit 22297c3db06b72b6c4b3ece0718783f98d9d04c6.

--- Source/CounterStrike/CounterStrike/UIElements/GameCanvas.cs
+++ Source/CounterStrike/CounterStrike/UIElements/GameCanvas.cs
@@ -3,53 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Threading;
 
 namespace CounterStrike.UIElements
 {
-    public class GameCanvas : Control
+    public class GameCanvas : Canvas
     {
-        public static DependencyProperty DataProperty;
-
-        private DispatcherTimer timer;
-
-        static GameCanvas()
-		{
-			//This OverrideMetadata call tells the system that this element wants to provide a style that is different than its base class.
-			//This style is defined in themes\generic.xaml
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(GameCanvas), new FrameworkPropertyMetadata(typeof(GameCanvas)));
-            DataProperty = DependencyProperty.Register("OX",typeof(int), typeof(GameCanvas));
-        }
-
-        public int OX
-        {
-            get
-            {
-                return (int)GetValue(DataProperty);
-            }
-            set
-            {
-                SetValue(DataProperty, value);
-            }
-        }
-
-        protected override void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-
-            OX = 300;
-
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(100);
-            timer.Tick += new EventHandler(Timer_Tick);
-            timer.Start();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            OX += 25;
-        }
     }
 }
